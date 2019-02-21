94a68f3d682668e35e442cb2fe5144d51fea1893
Artemy Vysotsky <artemyv@gmail.com>
2019-02-21 21:48:37 +0200

    simplified pending list and wrapper

--- test1/test1.cpp
+++ test1/test1.cpp
@@ -2,8 +2,28 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <utility>
+#include <list>
 
-std::future<int> test()
+template<class Func, class ...Args>
+std::pair<bool,bool> wrapper(std::list<std::future<int>>& peding, Func f, Args... args) {
+    bool bAsync = false;
+    if (bAsync)
+    {
+        auto task = std::async(std::launch::async, f, std::forward<Args>(args)...);
+        if (task.wait_for(std::chrono::milliseconds(100)) == std::future_status::ready)
+        {
+            return std::make_pair(true, true);
+        }
+        peding.emplace_back(std::move(task));
+        return std::make_pair(false, true);
+    }
+    else
+    {
+        return std::make_pair(true, false); //Sync - call function outside wrapper
+    }
+}
+void test(std::list<std::future<int>>& peding)
 {
     struct Return
     {
@@ -13,31 +33,66 @@ std::future<int> test()
     };
     std::promise<Return> ret_val;
     auto local = ret_val.get_future();
+
     auto func = [](std::promise<Return>&& ret_val) -> int
     {
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
         ret_val.set_value({ "Hello", 5, {1,2,3,4,5} });
         return 0;
     };
-
-    auto task = std::async(std::launch::async, func, std::move(ret_val));
-    if (local.wait_for(std::chrono::milliseconds(100)) == std::future_status::ready)
+    auto result = wrapper(peding, func, std::move(ret_val));
+    if (!result.second)
     {
-        auto i = local.get();
-        std::cout << "call local=" << i.msg << "\n";
+        std::cout << "making sync call1\n";
+        return;
     }
 
-    return task;
+    if (local.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready)
+    {
+        auto i = local.get();
+        std::cout << "call1 local=" << i.msg << "\n";
+    }
+
+    return;
+}
+
+void test1(std::list<std::future<int>>& peding)
+{
+    auto func = []() -> int
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        return 0;
+    };
+    auto result = wrapper(peding, func);
+    if (!result.second)
+    {
+        std::cout << "making sync call2\n";
+        return;
+    }
+
+    if (result.first)
+    {
+        std::cout << "async call2 succeded\n";
+    }
+
+    return;
 }
 
 int main()
 {
+    std::list<std::future<int>> peding;
     auto start = std::chrono::steady_clock::now();
-    auto res = test();
+    test(peding);
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>
         (std::chrono::steady_clock::now() - start);
     std::cout << "call finished: " << duration.count() << "ms\n";
-    res.wait_for(std::chrono::milliseconds(2000));
+    test1(peding);
+    std::cout << "call2 finished: " << duration.count() << "ms\n";
+
+    for (auto& p : peding)
+    {
+        p.wait();
+    }
     duration = std::chrono::duration_cast<std::chrono::milliseconds>
         (std::chrono::steady_clock::now() - start);
     std::cout << "wait finished: " << duration.count() << "ms\n";
